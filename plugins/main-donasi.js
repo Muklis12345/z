4cabc7127a03ced55f1dd2ce160db3ed7ed0aee7
Muklis12345 <114465542+Muklis12345@users.noreply.github.com>
2022-10-19 12:46:10 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -2,13 +2,12 @@ const qrmursid = "https://telegra.ph/file/c8a8cabe89cda01e0fec9.jpg"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrmursid, `
 ┏──「 Donasi • E-money 」─⬣
-│ • Gopay : 088233832771
-│ • Dana : 088233832771
-│ • Smartfren : 088233832771
-│ • Tri : 089674452300
+│ • Gopay : 085838571629
+│ • Dana : 085896661014
+│ • Indosat : 085838571629
 ┗────────⬣
 ┏──「 *NOTE* 」─⬣
-│ > Ingin donasi? wa.me/6288233832771
+│ > Ingin donasi? wa.me/6285838142263
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala, siapapun yang donasi_
